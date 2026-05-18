a046bbef9532c3d822640abf8444852149ea8db9
HenriqueAgarb <luishenrique_agarb@hotmail.com>
2026-05-18 20:47:40 -0300

    calculator python
    first version of calculator in python

---
+++ projeto2_calculadora.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Preparando o ambiente para execução"
+sudo apt install python3
+sudo apt update
+echo "Iniciando a calculadora"
+python3 ./projeto_1.py
+echo "Programa finalizado"
+
